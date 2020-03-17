ac0f5c55ca353485d3b0d4a17bc177a9f8b3b8a9
msegnit <melsegnit@gmail.com>
2020-03-17 17:20:00 -0500

    second commit

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Puppies</h1>')
